c67dae9101b22f47d5b3f3c6d195ab41cc5eb389
Evgen_Palimov <palbimov@mail.ru>
2021-01-23 14:11:03 +0500

    Lesson-4, Home Work is ready

---
+++ Lesson-4/Task-2.py
@@ -0,0 +1,14 @@
+"""
+Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
+"""
+
+def max_number():
+  user_answer = input("Введите 3 числа через пробел: ")
+  my_list = []
+  for el in range(user_answer.count(" ") + 1):
+    my_list = user_answer.split()
+
+  result = max(my_list)
+  return result
+
+print(max_number())
